8cf5d37bc74ad86dddb80a5a37b77c6437d27dee
owaisrazads <oraza7969@gmail.com>
2024-06-01 15:24:03 +0500

    two screens

--- src/config/Routing.jsx
+++ src/config/Routing.jsx
@@ -4,16 +4,20 @@ import BigBanner from '../pages/BigBanner'
 import Renegade from '../pages/Renegade'
 import Renegade2 from '../pages/Renegade2'
 import Renegade3 from '../pages/Renegade3'
+import TimeFrame from '../pages/TimeFrame'
+import FdaProcess from '../pages/FdaProcess'
 
 const Routing = () => {
   return (
     <>
         <BrowserRouter>
             <Routes>
+              <Route path='/timeFrame' element={<TimeFrame/>}/>
                 <Route path='/bigBanner' element={<BigBanner/>}/>
                 <Route path='/renegade' element={<Renegade/>}/>
                 <Route path='/renegade2' element={<Renegade2/>}/>
                 <Route path='/renegade3' element={<Renegade3/>}/>
+                <Route path='/fdaProcess' element={<FdaProcess/>}/>
             </Routes>
         </BrowserRouter>
     </>
